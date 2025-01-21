9e593926b9a42b5ce96106143876807b4fb3e8f7
NickieUwU <vlastart.cze555@gmail.com>
2025-01-21 22:37:22 +0100

    Reworked the opening tag logic

--- WML/compiler/compiler.cpp
+++ WML/compiler/compiler.cpp
@@ -6,17 +6,19 @@ void compile(ifstream &WML_FILE, ofstream &HTML_FILE, string &content)
     while(getline(WML_FILE, content))
     {
         string openingComponent = getOpeningComponent(content);
-        if(emptyComponent(openingComponent)) continue;
-        openingComponentHandler(HTML_FILE, openingComponent);
+        openingComponentHandler(HTML_FILE, content, openingComponent);
     }
 }
 
-void openingComponentHandler(ofstream &HTML_FILE, string &component)
+void openingComponentHandler(ofstream &HTML_FILE, string &content, string &component)
 {
-    HTML_FILE << "<"<<component.substr(0, component.size()-2);
+    if(content.find(component)!=string::npos&&!emptyComponent(component))
+    {
+        HTML_FILE << "<"+component.substr(0, component.size()-2);
+    }
 }
 
 bool emptyComponent(string &component)
 {
-    return component.empty() ?true:false;
+    return component.empty();
 }
--- WML/compiler/compiler.h
+++ WML/compiler/compiler.h
@@ -2,6 +2,6 @@
 #ifndef COMPILER_H
 #define COMPILER_H
     void compile(ifstream &WML_FILE, ofstream &HTML_FILE, string &content);
-    void openingComponentHandler(ofstream &HTML_FILE, string &component);
+    void openingComponentHandler(ofstream &HTML_FILE, string &content, string &component);
     bool emptyComponent(string &component);
 #endif
