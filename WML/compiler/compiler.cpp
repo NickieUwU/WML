d9c843cb9cbebc74b30297d27e786fd85bd3deeb
NickieUwU <vlastart.cze555@gmail.com>
2025-01-21 22:28:54 +0100

    the tag is now opening

--- WML/compiler/compiler.cpp
+++ WML/compiler/compiler.cpp
@@ -1,14 +1,22 @@
 #include "compiler.h"
 #include "../components/components.h"
+
 void compile(ifstream &WML_FILE, ofstream &HTML_FILE, string &content)
 {
     while(getline(WML_FILE, content))
     {
         string openingComponent = getOpeningComponent(content);
+        if(emptyComponent(openingComponent)) continue;
+        openingComponentHandler(HTML_FILE, openingComponent);
     }
 }
 
-void openingComponentHandler(ofstream &HTML_FILE, string &openingComponent)
+void openingComponentHandler(ofstream &HTML_FILE, string &component)
 {
-    
+    HTML_FILE << "<"<<component.substr(0, component.size()-2);
+}
+
+bool emptyComponent(string &component)
+{
+    return component.empty() ?true:false;
 }
--- WML/compiler/compiler.h
+++ WML/compiler/compiler.h
@@ -2,4 +2,6 @@
 #ifndef COMPILER_H
 #define COMPILER_H
     void compile(ifstream &WML_FILE, ofstream &HTML_FILE, string &content);
+    void openingComponentHandler(ofstream &HTML_FILE, string &component);
+    bool emptyComponent(string &component);
 #endif
